4c076e935aa47936385ad64ec52daf2528cb8e99
Альвина <alvina-b@yandex.ru>
2022-10-24 14:01:23 +0600

    решение задач с переменными

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        double dog = 8;
+    System.out.println(dog);
+    var cat = 3.6;
+    System.out.println(cat);
+   var paper = 763789;
+    System.out.println(paper);
+     dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+       dog = dog - 3.6;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var boxersWeight = boxerWeight1 + boxerWeight2;
+        System.out.println(boxersWeight);
+        var weightDifference = boxerWeight2 - boxerWeight1;
+        System.out.println(weightDifference);
+        var weightDifference1 = boxerWeight2 % boxerWeight1;
+        System.out.println(weightDifference1);
+
+        var totalHours = 640;
+        var employeeHours = 8;
+        var employeeNum = totalHours / employeeHours;
+        System.out.println("Всего работников в компании - " + employeeNum + " человек.");
+        var employeeNum1 = employeeNum + 94;
+        var totalHours1 = employeeHours * employeeNum1;
+        System.out.println("Если в компании работает " + employeeNum1 +
+                " человека, то всего " + totalHours1 + " часа работы может быть поделено между сотрудниками.");
+
+
+
+
+
+
+
+
+    }
+}
