79f613e3dbf0e6208f65435c39a21780819a65ed
Альвина <alvina-b@yandex.ru>
2022-10-26 14:40:56 +0600

    добавила слово "Задача"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача 1");
         double dog = 8;
     System.out.println(dog);
     var cat = 3.6;
     System.out.println(cat);
    var paper = 763789;
+        //Задача 2
+        System.out.println("Задача 2");
     System.out.println(paper);
      dog = dog + 4;
         System.out.println(dog);
@@ -12,18 +16,24 @@ public class Main {
         System.out.println(cat);
         paper = paper + 4;
         System.out.println(paper);
+        //Задача 3
+        System.out.println("Задача 3");
        dog = dog - 3.6;
         System.out.println(dog);
         cat = cat - 1.6;
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+        //Задача 4
+        System.out.println("Задача 4");
 var friend = 19;
         System.out.println(friend);
         friend = friend * 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        //Задача 5
+        System.out.println("Задача 5");
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
@@ -32,7 +42,8 @@ var friend = 19;
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
-
+        //Задача 6
+        System.out.println("Задача 6");
         var boxerWeight1 = 78.2;
         var boxerWeight2 = 82.7;
         var boxersWeight = boxerWeight1 + boxerWeight2;
@@ -41,7 +52,8 @@ var friend = 19;
         System.out.println(weightDifference);
         var weightDifference1 = boxerWeight2 % boxerWeight1;
         System.out.println(weightDifference1);
-
+        //Задача 7
+        System.out.println("Задача 7");
         var totalHours = 640;
         var employeeHours = 8;
         var employeeNum = totalHours / employeeHours;
